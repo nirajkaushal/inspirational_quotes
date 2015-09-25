ddb775c27469b015b163a072b5f3348bb0ed013b
Niraj Kaushal <nirajkaushal007@gmail.com>
2015-09-25 21:38:36 +0530

    create start.php file

---
+++ start.php
@@ -0,0 +1,9 @@
+<?php
+
+elgg_register_event_handler('init', 'system', 'inspirationalquote_init'); 
+
+function inspirationalquote_init() {        
+    elgg_register_widget_type('inspirationalquote', 'Inspirational Quote', 'The "Inspirational Quote" widget');
+}
+       
+?>
