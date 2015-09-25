f00792588f04cfbfb072d80e4dde318ae2b707d0
Niraj Kaushal <nirajkaushal007@gmail.com>
2015-09-25 21:49:21 +0530

    updated start.php file

--- start.php
+++ start.php
@@ -3,7 +3,7 @@
 elgg_register_event_handler('init', 'system', 'inspirationalquote_init'); 
 
 function inspirationalquote_init() {        
-    elgg_register_widget_type('inspirationalquote', 'Inspirational Quote', 'The "Inspirational Quote" widget');
+    elgg_register_widget_type('inspirationalquotes', 'Inspirational Quote', 'The "Inspirational Quote" widget');
 }
        
 ?>
