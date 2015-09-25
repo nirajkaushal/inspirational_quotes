dd33b4b263cdde5738897768b2e60c88feb2745a
Niraj Kaushal <nirajkaushal007@gmail.com>
2015-09-25 21:31:53 +0530

    create content.php file

---
+++ views/default/widgets/inspirational_quotes/content.php
@@ -0,0 +1,163 @@
+<center>
+<?php
+//Select random number form 1 to 50
+$num = Rand (1,50); 
+
+//Based on the random number, gives a quote 
+switch ($num)
+{
+case 1:
+echo "<b>Things work out best for those who make the best of how things work out.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>John Wooden</b></h3>";
+break;
+case 2:
+echo "<b>Dream Dream Dream, Dreams transform into thoughts and thoughts result in action.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Dr. Abdul Kalam</b></h3>";
+break;
+case 3:
+echo "<b>To live a creative life, we must lose our fear of being wrong.</b><BR> <b><h3 style='color:#782492;padding-top:5px'>Anonymous</b></h3>";
+break;
+case 4:
+echo "<b>If you are not willing to risk the usual you will have to settle for the ordinary.</b><BR> <b><h3 style='color:#782492;padding-top:5px'>Jim Rohn</b></h3>";
+break;
+case 5:
+echo "<b>Trust because you are willing to accept the risk, not because it's safe or certain.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 6:
+echo "<b>Take up one idea. Make that one idea your life--think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body, be full of that idea, and just leave every other idea alone. This is the way to success.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Swami Vivekananda</b></h3>";
+break;
+case 7:
+echo "<b>All our dreams can come true if we have the courage to pursue them.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Walt Disney</b></h3>";
+break;
+case 8:
+echo "<b>Good things come to people who wait, but better things come to those who go out and get them.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 9:
+echo "<b>If you do what you always did, you will get what you always got.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 10:
+echo "<b>Success is walking from failure to failure with no loss of enthusiasm.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Winston Churchill</b></h3>";
+break;
+case 11:
+echo "<b>Just when the caterpillar thought the world was ending, he turned into a butterfly.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Proverb</b></h3>";
+break;
+case 12:
+echo "<b>Successful entrepreneurs are givers and not takers of positive energy.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 13:
+echo "<b>Whenever you see a successful person you only see the public glories, never the private sacrifices to reach them.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Vaibhav Shah</b></h3>";
+break;
+case 14:
+echo "<b>Opportunities don't happen, you create them.</b><BR><h3 style='color:#782492;padding-top:5px'><b>Chris Grosser</b></h3>";
+break;
+case 15:
+echo "<b>Try not to become a person of success, but rather try to become a person of value.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Albert Einstein</b></h3>";
+break;
+case 16:
+echo "<b>Great minds discuss ideas, average minds discuss events, small minds discuss people.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Eleanor Roosevelt</b></h3>";
+break;
+case 17:
+echo "<b>I have not failed. I've just found 10,000 ways that won't work.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Thomas A. Edison</b></h3>";
+break;
+case 18:
+echo "<b>If you don't value your time, neither will others. Stop giving away your time and talents, start charging for it.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Kim Garst</b></h3>";
+break;
+case 19:
+echo "<b>A successful man is one who can lay a firm foundation with the bricks others have thrown at him.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>David Brinkley</b></h3>";
+break;
+case 20:
+echo "<b>No one can make you feel inferior without your consent.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Eleanor Roosevelt</b></h3>";
+break;
+case 21:
+echo "<b>The whole secret of a successful life is to find out what is one's destiny to do, and then do it.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Henry Ford</b></h3>";
+break;
+case 22:
+echo "<b>If you're going through hell keep going.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Winston Churchill</b></h3>";
+break;
+case 23:
+echo "<b>The ones who are crazy enough to think they can change the world, are the ones who do.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 24:
+echo "<b>Don't raise your voice, improve your argument.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 25:
+echo "<b>What seems to us as bitter trials are often blessings in disguise.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Oscar Wilde</b></h3>";
+break;
+case 26:
+echo "<b>The meaning of life is to find your gift. The purpose of life is to give it away.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 27:
+echo "<b>The distance between insanity and genius is measured only by success.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Bruce Feirstein</b></h3>";
+break;
+case 28:
+echo "<b>When you stop chasing the wrong things, you give the right things a chance to catch you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Lolly Daskal</b></h3>";
+break;
+case 29:
+echo "<b>Don't be afraid to give up the good to go for the great.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>John D. Rockefeller</b></h3>";
+break;
+case 30:
+echo "<b>No masterpiece was ever created by a lazy artist.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 31:
+echo "<b>Happiness is a butterfly, which when pursued, is always beyond your grasp, but which, if you will sit down quietly, may alight upon you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Nathaniel Hawthorne</b></h3>";
+break;
+case 32:
+echo "<b>If you can't explain it simply, you don't understand it well enough.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Albert Einstein</h3></b>";
+break;
+case 33:
+echo "<b>Blessed are those who can give without remembering and take without forgetting.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</h3></b>";
+break;
+case 34:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 35:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous<b/></h3>";
+break;
+case 36:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous<b/></h3>";
+break;
+case 37:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 38:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 39:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 40:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 41:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 42:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 43:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 44:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 45:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 46:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 47:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 48:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 49:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+case 50:
+echo "<b>Do one thing every day that scares you.</b><BR> <h3 style='color:#782492;padding-top:5px'><b>Anonymous</b></h3>";
+break;
+default:
+echo "<b>No Qoutes</b>";
+
+}
+?>
